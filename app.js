ddaddf802056c651dbc5bd2d5bb3d80f5f6809d5
Alwin Nedrum <nedrum@gmail.com>
2022-03-09 14:50:59 +0100

    Two boxes moving but buggy

--- app.js
+++ app.js
@@ -7,21 +7,36 @@ let initX = 0,
   newX,
   newY
 
-const handleClick = (e) => {
-  console.log(e)
-}
+let moveActive = false
 
-const changeBoxPosition = (posX, posY) => {
-  box.style.left = posX + 'px'
-  box.style.top = posY + 'px'
+const handleClick = (e) => {}
+
+const changeBoxPosition = (name, posX, posY) => {
+  console.log(name)
+  document.getElementById(name).style.left = posX + 'px'
+  document.getElementById(name).style.top = posY + 'px'
+
+  //   moveActive = false
 }
 
 const HandleMouseBtn = (e) => {
-  console.log(e)
-  e.type === 'mouseup' ? changeBoxPosition(e.clientX, e.clientY) : ''
+  console.log(e.target.id, moveActive)
+  e.preventDefault
+  let targetId = e.target.id
+  if (e.target.id != 'container') {
+    e.type === 'mousedown' && !moveActive ? (moveActive = true) : null
+    //   e.type === 'click' && moveActive ? (moveActive = false) : null
+
+    e.type === 'mousemove' && moveActive
+      ? changeBoxPosition(targetId, e.clientX, e.clientY)
+      : ''
+
+    e.type === 'mouseup' && moveActive ? (moveActive = false) : null
+  } else return (moveActive = false)
 }
 
 // box.addEventListener('click', (e) => handleClick(e))
 // container.addEventListener('click', (e) => handleClick(e))
 container.addEventListener('mousedown', (e) => HandleMouseBtn(e))
 container.addEventListener('mouseup', (e) => HandleMouseBtn(e))
+container.addEventListener('mousemove', (e) => HandleMouseBtn(e))
